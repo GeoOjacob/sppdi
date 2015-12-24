97a9e1b5c02b15ef98fabd75155aef4a608d1263
math-4lves <mluiz03369@gmail.com>
2015-12-24 00:40:39 -0200

    flyout funcionando

--- sistemaCorporativo/FORMS/PrincipalScreen.xaml.cs
+++ sistemaCorporativo/FORMS/PrincipalScreen.xaml.cs
@@ -17,6 +17,7 @@ using MahApps.Metro.Behaviours;
 using sistemaCorporativo;
 using sistemaCorporativo.FORMS.cadAgente;
 using sistemaCorporativo.FORMS;
+using sistemaCorporativo.UTIL.nameAndLastName;
 using MahApps.Metro.Actions;
 using MahApps.Metro.Converters;
 using MahApps.Metro;
@@ -40,14 +41,20 @@ namespace sistemaCorporativo.FORMS.principalScreen
             user = usuario.ToString();
         }
 
-        //String para buscar informações do usuario
-        private string SQL_SEARCH = "select id_Agente from login_Agente where nome_User = :user";
         //Criar string com o endereço do banco
         private string oradb = "Data Source=(DESCRIPTION="
                + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))"
                + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl.itb.com)));"
                + "User Id=matheus_23177;Password=123456;";
-
+        //Strings para informações do usuario
+        int casosResolvidos;
+        string cargo;
+        string idAgente;
+        string name;
+        string nivel;
+        string idCargo;
+        
+        
         private void AgenteMenuItem_Click(object sender, RoutedEventArgs e)
         {
             CadAgente cadAgente = new CadAgente();
@@ -232,12 +239,49 @@ namespace sistemaCorporativo.FORMS.principalScreen
         private void PrincipalScreen1_Loaded(object sender, RoutedEventArgs e)
         {
             OracleConnection Oracon = new OracleConnection(oradb);
+
+            //String para buscar informações do usuario
+            string SQL_SEARCH = "select id_Agente from login_Agente where nome_User = '" + user + "'";
             Oracon.Open();
 
-            OracleCommand cmd = new OracleCommand(SQL_SEARCH, Oracon);
-            cmd.Parameters.Add("user", user);
-            OracleDataReader read = cmd.ExecuteReader();
+            OracleCommand comando = new OracleCommand(SQL_SEARCH, Oracon);
+            OracleDataReader read = comando.ExecuteReader();
             read.Read();
+            idAgente = Convert.ToString(read[0].ToString());
+
+            //String para buscar informações do usuario
+            string SQL_SEARCH_ADVANCED = "select a.id_Agente, a.nome, l.nome_User, l.cargo_id_cargo from agente a Join LOGIN_AGENTE l on (l.id_Agente = '" + idAgente + "' and l.ID_AGENTE = a.ID_AGENTE and a.status != 0)";
+            string SQL_PROFILE_SEARCH = "select nivel_Agente, casos_resolvidos from perfil_Agente where id_Agente ='"+idAgente+"'";
+            OracleCommand cmdConsulta = new OracleCommand(SQL_SEARCH_ADVANCED, Oracon);
+            OracleCommand cmdProfile = new OracleCommand(SQL_PROFILE_SEARCH, Oracon);
+            OracleDataReader reader = cmdConsulta.ExecuteReader();
+            OracleDataReader rProfile = cmdProfile.ExecuteReader();
+            reader.Read();
+            rProfile.Read();
+
+            name = Convert.ToString(reader[1].ToString());
+            idCargo = Convert.ToString(reader[3].ToString());
+            nivel = Convert.ToString(rProfile[0].ToString());
+            casosResolvidos = Convert.ToInt32(rProfile[1].ToString());
+
+            string SQL_SEARCH_CARGO = "select id_Cargo, nome_Cargo from Cargo where status != 0 and ID_CARGO ='" + idCargo + "'";
+            OracleCommand cmdCargo = new OracleCommand(SQL_SEARCH_CARGO, Oracon);
+            OracleDataReader rCargo = cmdCargo.ExecuteReader();
+            rCargo.Read();
+
+            cargo = Convert.ToString(rCargo[1].ToString());
+
+            Oracon.Close();
+
+            name = nameAndLastName.FormataNome(name);            
+        }
+
+        private void FlyoutAgente_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblNomeAgente.Content = name.ToString();
+            lblNivel.Content = nivel.ToString();
+            lblCargo.Content = cargo.ToString();
+            lblCasosResolvidos.Content = casosResolvidos.ToString();
         }	  
     }
 }
---
+++ sistemaCorporativo/UTIL/nameAndLastName.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaCorporativo.UTIL.nameAndLastName
+{
+    class nameAndLastName
+    {
+        public static string FormataNome(string nome)
+        {
+            int primeiro, segundo, terceiro;
+            string completo;
+            string nome1, nome2, nome3, nome4;
+
+            primeiro = nome.IndexOf(" ");
+
+            nome1 = nome.Substring(0, primeiro + 1);
+            nome2 = nome.Substring(primeiro + 1, (nome.Length - (primeiro + 1)));
+
+            completo = nome1.Trim() + " " + nome2;
+
+            segundo = nome2.IndexOf(" ");
+
+            if (segundo > 0)
+            {
+                nome3 = nome2.Substring(segundo + 1, (nome2.Length - (segundo + 1)));
+                nome2 = nome2.Substring(0, segundo);
+
+                if (nome2.Length <= 3)
+                {
+                    terceiro = nome3.IndexOf(" ");
+
+                    if (terceiro > 0)
+                    {
+                        // achei um espaço
+                        nome4 = nome3.Substring(0, terceiro + 1);
+                        completo = nome1.Trim() + " " + nome2 + " " + nome4.Trim();
+                    }
+                    else
+                    {
+                        // n achei, terminou o nome
+                        completo = nome1.Trim() + " " + nome2 + " " + nome3;
+                    }
+                }
+                else
+                {
+                    completo = nome1.Trim() + " " + nome2.Substring(0, segundo);
+                }
+            }
+
+            return completo;
+        }
+    }
+}
